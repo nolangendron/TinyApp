6440000c260c1f782fd1a688e851969ad12e6f39
nolangendron <nolanglengendron@gmail.com>
2019-06-05 18:51:24 +0000

    Cleaned up code to ensure working

--- express_server.js
+++ express_server.js
@@ -1,74 +1,83 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
-
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const PORT = 8080; // default port 8080
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+// storee (key)shortURL and (Value)longURL
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
+// Browse - Displays all URLs in urlDatabase
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Displays a form to add new URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Displays one URL to page
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
-
+ // Will redirect to the longURL webpage
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res, callback) => {// Log the POST request body to the console
-  console.log(req.body)
+// Adds a new URL to urlDatabase
+app.post("/urls", (req, res, callback) => {
   const randomString = generateRandomString();
   const newLongURL = req.body.longURL;
   urlDatabase[randomString] = newLongURL;
-  res.redirect(`/urls/${randomString}`);         // Respond with 'Ok' (we will replace this)
+      res.redirect(`/u/${randomString}`);
 });
 
-app.post("/urls/:shortURL/delete", (req, res, callback) => {
-  const id = req.params.shortURL;
-  console.log(id);
-  delete urlDatabase[id];
-  console.log(urlDatabase);
-  res.redirect("/urls")
+// Edit/Update URL
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  if (longURL) {
+    urlDatabase[shortURL] = longURL;
+  }
+  res.redirect("/urls");
+;})
+
+// Delete a URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 })
+
+// catchall route
+app.get('*', (req, res) => {
+  res.redirect('/urls');
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 function generateRandomString() {
-  var output = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let output = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
     output += characters.charAt(Math.floor(Math.random() * characters.length))
   }
